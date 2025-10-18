8915d52a65ecc891feae345a1c73cc553ff124f8
Hasnine670817 <hasninetorun@gmail.com>
2025-10-18 09:58:48 +0600

    problem-6 done

---
+++ problem-6.js
@@ -0,0 +1,15 @@
+/*
+🧩 Problem 2: Roll Two Dice and Show the Total
+
+Question:
+Simulate rolling two dice. Generate two random numbers between 1 and 6, and return their sum.
+*/
+
+function rollDice() {
+    let dice1 = Math.floor(Math.random() * 6) + 1;
+    let dice2 = Math.floor(Math.random() * 6) + 1;
+    console.log(dice1, dice2);
+    return dice1 + dice2;
+}
+
+console.log(rollDice());
