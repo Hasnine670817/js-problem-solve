5397822a2df8b2dd433edcbd2a8385382051f3d5
Hasnine670817 <hasninetorun@gmail.com>
2025-10-18 09:55:07 +0600

    problem-5.js

---
+++ problem-5.js
@@ -0,0 +1,13 @@
+/*
+🧩 Problem 1: Random Number Between 1 and 50
+
+Question:
+Write a function that returns a random number between 1 and 50.
+*/
+
+function randomNumber() {
+    let random = Math.floor(Math.random() * 50) + 1;
+    return random;
+}
+
+console.log(randomNumber());
